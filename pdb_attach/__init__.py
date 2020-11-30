22cf87df31154973b830b059ae2370e6f845f482
Spencer Mitchell <smitchell556@gmail.com>
2020-11-30 17:47:44 -0500

    fix: use correct path for VERSIONS.txt

--- pdb_attach/__init__.py
+++ pdb_attach/__init__.py
@@ -1,8 +1,10 @@
 # -*- mode: python -*-
 """pdb-attach is a python debugger that can attach to running processes."""
+import os
+
 from .pdb_detach import listen, unlisten
 
 __all__ = ["listen", "unlisten"]
 
-with open("VERSION.txt") as f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "VERSION.txt")) as f:
     __version__ = f.read().strip()
