341aaf2af33c73afec928d7c3e5be15a7aa5ba15
Spencer Mitchell <smitchell556@gmail.com>
2020-12-12 13:36:37 -0500

    fixup! fix: raise a warning on Windows

--- test/test_windows_import.py
+++ test/test_windows_import.py
@@ -11,23 +11,10 @@ warnings.simplefilter("always")
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 
 
-def test_import_raises_warning():
-    """Test importing pdb_attach raises a warning."""
-
-    def import_pdb_attach():
-        """Pytest doesn't catch warnings emitted by imports well.
-
-        It can catch them from function calls though.
-        """
-        import pdb_attach  # noqa: F401
-
-    with pytest.warns(UserWarning):
-        import_pdb_attach()
-
-
 def test_listen_raises_warning():
-    """Test calling listen and unlisten raise a warning."""
-    import pdb_attach
+    """Test warnings are raised."""
+    with pytest.warns(UserWarning):
+        import pdb_attach
 
     with pytest.warns(UserWarning):
         pdb_attach.listen(50000)
