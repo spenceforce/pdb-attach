c8146f22138914fc806c1825093ec4ad88558248
Spencer Mitchell <smitchell556@gmail.com>
2021-08-08 19:03:42 -0400

    feat: client side API
    This commit implements a few methods that are useful for scripting
    client side interactions. Namely the `PdbClient` class in
    `pdb_attach/client.py`.
    
    Refs #12

--- test/context.py
+++ test/context.py
@@ -11,4 +11,6 @@ import pytest
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir)))
 import pdb_attach
+import pdb_attach.client as pdb_client
 import pdb_attach.pdb_detach as pdb_detach
+from pdb_attach._prompt import PROMPT
